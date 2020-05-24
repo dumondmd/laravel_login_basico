c7ec67020463613a636b89f01cb78ed6bcdb905d
Dumon <dumon.dmd@gmail.com>
2020-05-24 10:53:28 -0300

    Redirecionando do login para outra página

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/produtos', 'ProdutoControlador@index')
-		->middleware('auth')
+Route::get('/produtos', 'ProdutoControlador@index')		
 		->name('produtos');
